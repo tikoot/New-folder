49b946e7175d9c9165e4c0afc891f14df8ac4c5d
tiko.otarashvili99@gmail.com <tiko.otarasvhili99@gmail.com>
2022-06-19 17:33:02 +0200

    better version

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-let myImages = ["image1.jpg","image2.jpg","image3.jpg","image4.jpg","image5.jpg"];
 let currentImage = 0;
+let myPhotos = ["image1.jpg","image2.jpg","image3.jpg","image4.jpg","image5.jpg"];
+let container = document.getElementById('content');
+let nextBtn = document.getElementById('next');
+let previousBtn = document.getElementById('previous');
 
-document.getElementById('next').onclick = nextPhoto;
-document.getElementById('previous').onclick = previousPhoto;
 
-function nextPhoto(){
-    currentImage++;
+nextBtn.addEventListener('click' , function(event){
+        event.preventDefault();
 
-    if(currentImage > myImages.length-1){
-        currentImage = 0;
-    }
+        currentImage++;
+        if(currentImage > myPhotos.length-1){
+            currentImage = 0; 
+        }
+        swap();
+       
+});
 
-    document.getElementById('myimage').src = myImages[currentImage];
-}
- 
+previousBtn.addEventListener('click' , function(event){
+    event.preventDefault();
 
-function previousPhoto(){
     currentImage--;
-
     if(currentImage < 0){
-        currentImage = myImages.length-1;
+        currentImage = myPhotos.length-1; 
     }
 
-    document.getElementById('myimage').src = myImages[currentImage];
+    swap();
+});
+
+function swap(){
+    let newSlide = document.createElement('img');
+    newSlide.src = `slides/${myPhotos[currentImage]}`;
+    newSlide.className = 'fadeinimg';
+    container.appendChild(newSlide);
+
+    if(container.children.length > 2){
+        container.removeChild(container.children[0]);
+    }
 }
- 
